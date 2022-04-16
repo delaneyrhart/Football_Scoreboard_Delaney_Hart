fc39e41140ac2e812c95ff260121defe2e3d3583
Delaney R. Hart <102171684+delaneyrhart@users.noreply.github.com>
2022-04-16 01:12:41 +0000

    Mostly finished Team class

--- main.cpp
+++ main.cpp
@@ -14,19 +14,28 @@ class Team
     string name;
     string coach_Name;
     string home_City;
-    string home_Status;
+    bool home_Status;
     int score;
     int timeout_Count;
   public:
     Team() //default constructor
-{
-  score = 0; 
+    {
+        score = 0; 
         home_Status = false; //visitor = false, home = true
         name = "DefaultTeamName"; 
         timeout_Count = 0; //I don't actually know what this is
         coach_Name = "DefaultCoachName"; 
-}
-  
+     }
+    void setScore(int s) { score = s; }
+    void setHomeStatus(bool hs) { home_Status = hs; }
+    void setName(string n) { name = n; }
+    void setShotsOnGoal(int tc) {  timeout_Count = tc; }
+    void setCoachName(string cn) { coach_Name = cn; }
+    int getScore() const { return score; }
+    bool getHomeStatus() const { return home_Status; }
+    string getName() const { return name; }
+    int getShotsOnGoal() const { return timeout_Count; }
+    string getCoachName() const { return coach_Name; }
 };
 
 class Scoreboard
