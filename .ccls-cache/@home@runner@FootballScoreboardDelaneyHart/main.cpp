e3ed09ccf8d086a61ac852a986e914ca5d3088dc
Delaney R. Hart <102171684+delaneyrhart@users.noreply.github.com>
2022-04-17 00:56:53 +0000

    This might have been a mistake...

--- .ccls-cache/@home@runner@FootballScoreboardDelaneyHart/main.cpp
+++ .ccls-cache/@home@runner@FootballScoreboardDelaneyHart/main.cpp
@@ -11,29 +11,38 @@ using namespace std;
 class Team
 {
   private:
-    string name;
-    string coach_Name;
+    string name1;
+    string name2;
+    string home_coachName;
+    string visitor_coachName;
     string home_City;
+    string visitor_City;
     bool home_Status;
     int score;
     int timeout_Count;
   public:
     Team() //default constructor
     {
-        name = "Default Team Name";
-        coach_Name = "Default Coach";
+        name1 = "Longhorns";
+        name2 = "Red Raiders";
+        home_coachName = "Joey McGuire";
+        visitor_coachName = "";
+        home_City = "Austin";
+        visitor_City = "Lubbock";
         score = 0; 
         home_Status = false; //visitor = false, home = true
         timeout_Count = 0; //I don't actually know what this is
      }
     void setScore(int s) { score = s; }
     void setHomeStatus(bool hs) { home_Status = hs; }
-    void setName(string n) { name = n; }
+    void setName1(string n1) { name1 = n1; }
+    void setName2(string n2) {name2 = n2; }
     void setShotsOnGoal(int tc) {  timeout_Count = tc; }
     void setCoachName(string cn) { coach_Name = cn; }
     int getScore() const { return score; }
     bool getHomeStatus() const { return home_Status; }
-    string getName() const { return name; }
+    string getName1() const { return name1; }
+    string getName2() const { return name2; }
     int getShotsOnGoal() const { return timeout_Count; }
     string getCoachName() const { return coach_Name; }
 };
@@ -67,7 +76,7 @@ class Scoreboard
       string score = "\x1b[36;4m"; //blue for teams
       string title = "\x1b[46;1m"; //title
       cout << title << "\t\tFOOTBALL SCOREBOARD PROGRAM\n\n" << reset << endl;
-      cout << " " << score << home.getName() << reset << "\t\t\t\t" << score << visitor.getName() << reset << endl; //to keep track of teams
+      cout << " " << score << home.getName1() << reset << "\t\t\t\t" << score << visitor.getName2() << reset << endl; //to keep track of teams
       cout << " " << home.getCoachName() << "\t\t\t\t\t" << visitor.getCoachName() << endl; //keep track of coaches
       cout << " _______________________________________________" << endl;
       cout << "|     HOME" << "                          " 
@@ -83,11 +92,11 @@ class Scoreboard
        cout << "Home> \t"; 
        if(home.getHomeStatus() == true)
        {
-         cout << "Team 1: " << home.getName() << "*"; 
+         cout << "Team 1: " << home.getName1() << "*"; 
        }
        else if(visitor.getHomeStatus() == true)
        {
-         cout << "Team 2: " << visitor.getName() << "*"; 
+         cout << "Team 2: " << visitor.getName2() << "*"; 
        }
        else
        {
@@ -117,7 +126,6 @@ int main() {
       system("clear"); //clear the screen of previous content 
       sb.showScoreboard(); //show the current scoreboard data
       //menu options 
-      cout << "A = Change Home Team" << endl;
       cout << "A = Update Home Team Name" << endl; 
       cout << "B = Update Home Team Coach" << endl; 
       cout << "C = Update Home Team Score" << endl;
@@ -132,17 +140,24 @@ int main() {
 
       if(userChoice == "A" || userChoice == "a")
       {
-        //Dr_T challenge Accept a new name for s's home team
-      
-        //change that home team's default name
-        team1.setName(newName); //set tOne's data to the new desired name
-      }
-      else if(userChoice == "B" || userChoice == "b")
-      {
-        cout << "\nUpdate Home Score Module****" << endl; 
-        cout << "\nPlease enter a new score for the home team: "; 
-        cin >> newScore; 
-        team1.setScore(newScore);  //set the new score for tOne        
+        cout << "\nChoose the Home Team" << endl; 
+        cout << "\nWho is the home team? 1 = Team 1, 2 = Team 2: "; 
+        homeTeamQuestion = validateInt(homeTeamQuestion); 
+       // cin >> homeTeamQuestion; 
+        if(homeTeamQuestion == 1)
+        {
+          team1.setHomeStatus(true); 
+          team2.setHomeStatus(false); 
+        }
+        else if(homeTeamQuestion == 2)
+        {
+          team1.setHomeStatus(false); 
+          team2.setHomeStatus(true);
+        }
+        else
+        {
+          cout << "\nInvalid Input!" << endl; 
+        }
       }
       else if(userChoice == "C" || userChoice == "c")
       {
